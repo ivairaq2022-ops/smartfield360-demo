8200ebfeb898e56f3b34d0dec32dc6ff630e2dc1
NJ <iva.iraq2022@gmail.com>
2026-09-19 19:58:57 +0300

    Cache dedicated Arabic interface

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='smartfield360-v5';
-const ASSETS=['./','./index.html','./manifest.json','./client-config.js','./icons/icon.svg'];
+const CACHE='smartfield360-v6';
+const ASSETS=['./','./index.html','./ar.html','./manifest.json','./client-config.js','./icons/icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{
