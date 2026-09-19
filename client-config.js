0bd6d24ce292624ca234de0363a83750226283ba
NJ <iva.iraq2022@gmail.com>
2026-09-19 13:53:40 +0300

    Add client-configurable oilfield profile

---
+++ client-config.js
@@ -0,0 +1,13 @@
+window.CLIENT_CONFIG = {
+  clientName: "Client Oilfield",
+  fieldName: "Integrated Oilfield Operations",
+  country: "Iraq",
+  confidentiality: "Confidential Client Demonstration",
+  preparedBy: "Smart Innovation for Engineering & IT",
+  currency: "USD",
+  wells: 8,
+  theme: {
+    primary: "#d71920",
+    dark: "#111318"
+  }
+};
