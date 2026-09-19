00fc08dfeb0e2b7a1e0b6b8dc100a5c965da8fbb
NJ <iva.iraq2022@gmail.com>
2026-09-19 20:08:38 +0300

    Refresh cache after complete language cleanup

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='smartfield360-v8';
+const CACHE='smartfield360-v9';
 const ASSETS=['./','./index.html','./en.html','./ar.html','./manifest.json','./client-config.js','./icons/icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
