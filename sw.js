2a9af7bcdaf4c98e5f7451232480f7e65e6dfd99
NJ <iva.iraq2022@gmail.com>
2026-09-19 17:18:17 +0300

    Force PWA cache refresh for new client features

--- sw.js
+++ sw.js
@@ -1,1 +1,8 @@
-const CACHE='smartfield360-v2';const ASSETS=['./','./index.html','./manifest.json','./icons/icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='smartfield360-v4';
+const ASSETS=['./','./index.html','./manifest.json','./client-config.js','./icons/icon.svg'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener('fetch',e=>{
+ if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return r}).catch(()=>caches.match('./index.html')));return}
+ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
