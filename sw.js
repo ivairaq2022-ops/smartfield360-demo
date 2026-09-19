047a446bc5239e729d0ab88194ee5ac942920612
NJ <iva.iraq2022@gmail.com>
2026-09-19 20:10:40 +0300

    Fix language page navigation caching

--- sw.js
+++ sw.js
@@ -1,8 +1,25 @@
-const CACHE='smartfield360-v9';
+const CACHE='smartfield360-v10';
 const ASSETS=['./','./index.html','./en.html','./ar.html','./manifest.json','./client-config.js','./icons/icon.svg'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener('install',e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
 self.addEventListener('fetch',e=>{
- if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return r}).catch(()=>caches.match('./index.html')));return}
- e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+  const req=e.request;
+  const url=new URL(req.url);
+  if(req.mode==='navigate'){
+    e.respondWith(
+      fetch(req).then(r=>{
+        const copy=r.clone();
+        caches.open(CACHE).then(c=>c.put(req,copy));
+        return r;
+      }).catch(()=>caches.match(req).then(r=>r||caches.match('./index.html')))
+    );
+    return;
+  }
+  e.respondWith(caches.match(req).then(r=>r||fetch(req)));
 });
