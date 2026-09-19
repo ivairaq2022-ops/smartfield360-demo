f73b8b53c0925abd493b6e672b43a700e6616ec4
NJ <iva.iraq2022@gmail.com>
2026-09-19 20:13:33 +0300

    Disable stale PWA caching during bilingual stabilization

--- sw.js
+++ sw.js
@@ -1,25 +1,9 @@
-const CACHE='smartfield360-v10';
-const ASSETS=['./','./index.html','./en.html','./ar.html','./manifest.json','./client-config.js','./icons/icon.svg'];
-self.addEventListener('install',e=>{
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
-});
+const CACHE='smartfield360-final-v1';
+self.addEventListener('install',e=>{self.skipWaiting();});
 self.addEventListener('activate',e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))));
   self.clients.claim();
 });
 self.addEventListener('fetch',e=>{
-  const req=e.request;
-  const url=new URL(req.url);
-  if(req.mode==='navigate'){
-    e.respondWith(
-      fetch(req).then(r=>{
-        const copy=r.clone();
-        caches.open(CACHE).then(c=>c.put(req,copy));
-        return r;
-      }).catch(()=>caches.match(req).then(r=>r||caches.match('./index.html')))
-    );
-    return;
-  }
-  e.respondWith(caches.match(req).then(r=>r||fetch(req)));
+  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
 });
